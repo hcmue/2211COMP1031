3e1a35fd1c9b9a21548fbe63b6b83882557e336a
Hien Luong <hyhien@gmail.com>
2022-09-29 08:28:08 +0700

    Register context and use

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,28 +4,31 @@ import './App.css';
 import { Login } from './components/Login';
 import { HomePage } from './components/Home';
 import { About } from './components/About';
+import { MyContextProvider } from './contexts/MyContext';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <nav>
-          <Link to='/about'>About</Link>
-          <Link to='/login'>Login</Link>
-          <Link to='/Home'>Home</Link>
-        </nav>
+      <MyContextProvider>
+        <BrowserRouter>
+          <nav>
+            <Link to='/about'>About</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/Home'>Home</Link>
+          </nav>
 
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
 
-        <Routes>
-          <Route exact path='/' element={<HomePage />} />
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/home' element={<HomePage />} />
-          <Route exact path='/login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/home' element={<HomePage />} />
+            <Route exact path='/login' element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </MyContextProvider>
     </div>
   );
 }
--- demo/src/components/Home.js
+++ demo/src/components/Home.js
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
+import { MyContext } from '../contexts/MyContext';
 export const HomePage = () => {
+    const { username, isLogged } = useContext(MyContext);
 
     return (
         <>
             <h2>MY HOMEPAGE</h2>
+            <div>
+                {isLogged && (
+                    <div>WELCOME MY PRO</div>
+                )}
+                {isLogged ? (
+                    <div>Chào mừng bạn <strong>{username}</strong> thăm trang của tui</div>
+                ) : (
+                    <div>Bạn chưa đăng nhập</div>
+                )}
+            </div>
         </>
     )
 }
