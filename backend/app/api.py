a13f9a89593f89da5f7d39ae95ec666795e5d033
Hien Luong <hyhien@gmail.com>
2022-10-21 07:48:25 +0700

    API GET

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,7 +1,41 @@
-from operator import imod
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cho tất cả các trang đều access API được
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+todos = [
+    {
+        "id": 1,
+        "item": "Read a book."
+    },
+    {
+        "id": 2,
+        "item": "Cycle around town."
+    }
+]
+
+
+@app.get("/todo", tags=["todos"])
+async def get_todos() -> dict:
+    return {"data": todos}
+
+
+@app.get("/todo/{id}", tags=["todos"])
+async def get_todos(id: int) -> dict:
+    for item in todos:
+        print(item)
+        if item["id"] == id:
+            return {"data": item}
+    raise HTTPException(status_code=404, detail=f"Item {id} not found")
 
 
 @app.get("/")
