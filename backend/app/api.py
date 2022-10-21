c80d4d8831b9471fba23ee7716a4a3ff5ff765cf
Hien Luong <hyhien@gmail.com>
2022-10-21 09:04:15 +0700

    PUT, POST, DELETE todo

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -35,6 +36,48 @@ async def get_todos(id: int) -> dict:
         print(item)
         if item["id"] == id:
             return {"data": item}
+    raise HTTPException(status_code=404, detail=f"Item {id} not found")
+
+
+class TodoItem(BaseModel):
+    id: int
+    item: str
+
+
+@app.post("/todo", tags=["todos"])
+async def add_todo(todo: TodoItem) -> dict:
+    for item in todos:
+        print(item)
+        if item["id"] == todo.id:
+            raise HTTPException(
+                status_code=400, detail=f"Item {todo.id} existed")
+    todos.append(todo)
+    return {
+        "data": {"Todo added."}
+    }
+
+
+@app.put("/todo/{id}", tags=["todos"])
+async def update_todo(id: int, body: TodoItem) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todo["item"] = body.item
+            return {
+                "data": f"Todo with id {id} has been updated."
+            }
+
+    raise HTTPException(status_code=404, detail=f"Item {id} not found")
+
+
+@app.delete("/todo/{id}", tags=["todos"])
+async def delete_todo(id: int) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todos.remove(todo)
+            return {
+                "data": f"Todo with id {id} has been removed."
+            }
+
     raise HTTPException(status_code=404, detail=f"Item {id} not found")
 
 
