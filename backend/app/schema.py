8ee49c06d6da4a398c5a321a224e332ad59105d1
Hien Luong <hyhien@gmail.com>
2022-10-27 08:37:16 +0700

    Add model

---
+++ backend/app/schema.py
@@ -0,0 +1,16 @@
+# File app/schema.py
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    email: str
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
